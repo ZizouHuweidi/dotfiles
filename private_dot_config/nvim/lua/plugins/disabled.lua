d8b6fd3098c10d370d47bbd99697ec3ba312fc1d
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2024-03-04 10:39:17 +0100

    first commit

---
+++ private_dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+
+  { "folke/tokyonight.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "gbprod/yanky.nvim", enabled = false},
+}
---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "html",
+        "json",
+        "lua",
+        "markdown",
+        "regex",
+        "c",
+        "cpp",
+        "zig",
+        "java",
+        "templ",
+      },
+    },
+  },
+}
