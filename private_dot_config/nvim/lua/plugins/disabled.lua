2850d25b6da5654b8dacd1cd8caa269cb193cb2d
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2025-05-01 19:43:18 +0200

    nvim cleanup

--- private_dot_config/nvim/lua/plugins/disabled.lua
+++ private_dot_config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,7 @@
 return {
 
-  { "folke/tokyonight.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  -- { "rcarriga/nvim-notify",  enabled = true },
-  { "gbprod/yanky.nvim", enabled = false },
+  -- { "gbprod/yanky.nvim", enabled = false },
   -- { "nvim-neo-tree/neo-tree.nvim", enabled = false }
+
+  { "folke/tokyonight.nvim", enabled = false },
 }
