d089f72c49dbe42895dc4793f9d7ed7609f2010a
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-03-10 08:18:55 +0200

    bash cleanup

--- setup/zen/brew.sh
+++ setup/zen/brew.sh
@@ -1,1 +1,5 @@
-brew install uv go rustup deno oven-sh/bun/bun nvm gh lazygit yt-dlp fastfetch cmus bbrew
+brew install uv go rustup
+brew install nvm deno
+brew install oven-sh/bun/bun
+brew install anomalyco/tap/opencode
+brew install gh lazygit yt-dlp fastfetch cmus bbrew
