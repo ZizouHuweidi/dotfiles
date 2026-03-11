ce6582a1b9ac3ac7df4af80726455c4171c23b5e
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-03-11 05:49:52 +0200

    fisher

--- setup/zen/brew.sh
+++ setup/zen/brew.sh
@@ -2,4 +2,4 @@ brew install uv go rustup
 brew install nvm deno
 brew install oven-sh/bun/bun
 brew install anomalyco/tap/opencode
-brew install gh lazygit yt-dlp fastfetch cmus bbrew
+brew install gh lazygit yt-dlp fastfetch fisher
