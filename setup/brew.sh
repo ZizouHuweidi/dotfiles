eb4c87dd53e5a696368bdfb68ac04459588d2017
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-03-14 22:30:10 +0200

    setup cleanup

---
+++ setup/brew.sh
@@ -0,0 +1,3 @@
+brew install uv go rustup
+brew install nvm deno node oven-sh/bun/bun
+brew install anomalyco/tap/opencode gh lazygit yt-dlp fastfetch fisher
--- setup/zen/brew.sh
+++
@@ -1,5 +0,0 @@
-brew install uv go rustup
-brew install nvm deno
-brew install oven-sh/bun/bun
-brew install anomalyco/tap/opencode
-brew install gh lazygit yt-dlp fastfetch fisher
