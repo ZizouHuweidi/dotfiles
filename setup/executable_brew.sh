5022a4608ac534e38fe65ded26b2ffa10e3723b6
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-03-22 10:14:21 +0200

    delegate more to brew

--- setup/executable_brew.sh
+++ setup/executable_brew.sh
@@ -1,4 +1,5 @@
 brew install uv go rustup
 brew install nvm deno node oven-sh/bun/bun
 brew install anomalyco/tap/opencode gh lazygit
-brew install yt-dlp fastfetch fisher btop
+brew install yt-dlp fastfetch fisher
+brew install btop gdu yazi
