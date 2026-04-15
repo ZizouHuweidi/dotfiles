3d990800970ca4f82b226b3840b3fa8c1a4a7fd2
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-04-15 13:31:22 +0200

    jdown

--- setup/executable_flatpak.sh
+++ setup/executable_flatpak.sh
@@ -1,2 +1,2 @@
 flatpak install flathub com.ranfdev.DistroShelf org.telegram.desktop com.github.taiko2k.tauonmb
-flatpak install com.transmissionbt.Transmission com.github.johnfactotum.Foliate
+flatpak install com.transmissionbt.Transmission com.github.johnfactotum.Foliate org.jdownloader.JDownloader
