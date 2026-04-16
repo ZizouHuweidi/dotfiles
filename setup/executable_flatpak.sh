134ab784f984265ec837d916c1c062500bed3e0d
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-04-16 02:22:57 +0200

    lutris

--- setup/executable_flatpak.sh
+++ setup/executable_flatpak.sh
@@ -1,2 +1,3 @@
 flatpak install flathub com.ranfdev.DistroShelf org.telegram.desktop com.github.taiko2k.tauonmb
 flatpak install com.transmissionbt.Transmission com.github.johnfactotum.Foliate org.jdownloader.JDownloader
+flatpak install net.lutris.Lutris
