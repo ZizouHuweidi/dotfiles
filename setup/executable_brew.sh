ebf2b8f213d3d3f0f4aff1a3ddd9759b91c3c418
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-04-24 17:19:23 +0200

    brew tap

--- setup/executable_brew.sh
+++ setup/executable_brew.sh
@@ -3,3 +3,6 @@ brew install nvm deno node oven-sh/bun/bun
 brew install anomalyco/tap/opencode gh lazygit
 brew install yt-dlp fastfetch fisher
 brew install btop gdu yazi broot
+# brew tap ublue-os/tap
+# brew install --cask visual-studio-code-linux
+# brew install --cask antigravity-linux
