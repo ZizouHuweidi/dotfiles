20772c590e7dc6713dd75053e0144c0b91746b83
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-04-28 20:07:13 +0200

    add starship to brew

--- setup/executable_brew.sh
+++ setup/executable_brew.sh
@@ -2,7 +2,7 @@ brew install uv go rustup
 brew install nvm deno node oven-sh/bun/bun
 brew install anomalyco/tap/opencode gh lazygit
 brew install yt-dlp fastfetch fisher
-brew install btop gdu yazi broot
+brew install btop gdu yazi broot starship
 # brew tap ublue-os/tap
 # brew install --cask visual-studio-code-linux
 # brew install --cask antigravity-linux
