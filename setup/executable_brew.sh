3188ba3ff29784a8cdb1bfcd6c64d6f2692cb0f4
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-05-09 12:37:39 +0200

    bun is buns

--- setup/executable_brew.sh
+++ setup/executable_brew.sh
@@ -1,5 +1,5 @@
 brew install uv go rustup
-brew install nvm deno node oven-sh/bun/bun
+brew install nvm deno node
 brew install anomalyco/tap/opencode gh lazygit
 brew install yt-dlp fastfetch fisher
 brew install btop gdu yazi broot starship
