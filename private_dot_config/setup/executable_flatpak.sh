15be3495a8b07ea4019ff5fbc6619ffd7c0e7fcb
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2026-06-29 12:46:11 +0200

    better dark mode handeling

--- private_dot_config/setup/executable_flatpak.sh
+++ private_dot_config/setup/executable_flatpak.sh
@@ -1,3 +1,5 @@
 flatpak install flathub com.ranfdev.DistroShelf org.telegram.desktop com.github.taiko2k.tauonmb
 flatpak install com.transmissionbt.Transmission com.github.johnfactotum.Foliate org.jdownloader.JDownloader
 flatpak install net.lutris.Lutris org.remmina.Remmina io.dbeaver.DBeaverCommunity
+
+# flatpak override --user --env=GTK_THEME=adw-gtk3-dark --env=QT_QPA_PLATFORMTHEME=gtk
